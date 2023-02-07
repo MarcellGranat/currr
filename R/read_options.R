40b498758d33383a6b2d99483d3c331035dec50d
MarcellGranat <granatcellmar98@gmail.com>
2023-02-07 09:47:35 +0100

    debug options

--- R/read_options.R
+++ R/read_options.R
@@ -1,27 +1,35 @@
-read_options <- function(options) {
+read_options <- function(x) {
   if (is.null(getOption("currr.workers"))) {
     workers <- 1 # default
+  } else {
+    workers <- getOption("currr.workers")
   }
-  if ("workers" %in% names(options)) {
-    workers <- options$workers
+  if ("workers" %in% names(x)) {
+    workers <- x$workers
   }
   if (is.null(getOption("currr.wait"))) {
     wait <- 1
+  } else {
+    wait <- getOption("currr.wait")
   }
-  if ("wait" %in% names(options)) {
-    wait <- options$wait
+  if ("wait" %in% names(x)) {
+    wait <- x$wait
   }
   if (is.null(getOption("currr.fill"))) {
     fill <- TRUE
+  } else {
+    fill <- getOption("currr.fill")
   }
-  if ("fill" %in% names(options)) {
-    fill <- options$fill
+  if ("fill" %in% names(x)) {
+    fill <- x$fill
   }
   if (is.null("currr.n_checkpoint")) {
     n_checkpoint <- 100
+  } else {
+    n_checkpoint <- getOption("currr.n_checkpoint")
   }
-  if ("n_checkpoint" %in% names(options)) {
-    n_checkpoint <- options$n_checkpoint
+  if ("n_checkpoint" %in% names(x)) {
+    n_checkpoint <- x$n_checkpoint
   }
 
   if (!workers %in% 1:100) stop("`workers` must be a positive integer, defining the number of core to use for parrallel computing.")
